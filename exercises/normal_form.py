51275dfd2ded9260985902de3266b9382036c1b8
Tim van der Molen <tbvdm@xs4all.nl>
2014-01-18 20:50:46 +0100

    Remove incomplete functions and test code.

--- exercises/normal_form.py
+++ exercises/normal_form.py
@@ -30,18 +30,3 @@ def negation_normal_form(f):
                 return ('and', negation_normal_form(('not', f[1][1])), negation_normal_form(('not', f[1][2])))
     else:
         return (f[0], negation_normal_form(f[1]), negation_normal_form(f[2]))
-
-def conjunctive_normal_form(f):
-    f = negation_normal_form(f)
-
-def make_main_connective_and(f):
-    i=0
-    while not f[i][0] = 'and'
-
-from random_formula import generate_random_formula
-
-for i in range(10):
-    f = generate_random_formula((2, 5))
-    print("original: ", f)
-    print("nnf:      ", negation_normal_form(f))
-    print()
