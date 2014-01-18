729fc5ae5612cd65dd262a242ce6c0153e0c97f1
Bill Noble <winobes@gmail.com>
2014-01-18 12:35:25 +0100

    changed the handling of unary operators

--- exercises/random_formula.py
+++ exercises/random_formula.py
@@ -1,12 +1,11 @@
 import random 
 
-def generate_random_formula(n_atoms_range, negation_probability, 
-                            possible_atoms = None, possible_operators = None):
+def generate_random_formula(n_atoms_range, possible_atoms = None, 
+                            possible_binary_operators = None):
     """
     Generates a random formula from:
         n_atoms_range - range tuple (first element smaller than seccond)
               The formula with have some number of atoms in this range.
-        negation_probablity - the probability that any given subformula 
               in the formula is negated.
         possible_atoms - a list of tuples whose first element is an
               atomic sentence and whose seccond element is the element's
@@ -18,22 +17,31 @@ def generate_random_formula(n_atoms_range, negation_probability,
               to [('and', 1/3), ('or', 1/3), ('arrow', 1/3)]
     """
     from string import ascii_lowercase
+     
+    unary_operators = {'not'}
+    binary_operators = {'and', 'or', 'arrow'}
 
     if (not n_atoms_range[0] > 0 or
         not n_atoms_range[0] <= n_atoms_range[1]):
         raise ValueError('bad range for number of atoms')
 
     n_atoms = random.randrange(*n_atoms_range)
+    # generate list of atoms if none given
     if possible_atoms == None:
         n_possibles = random.randrange(int(n_atoms/2), n_atoms)
-        possible_atoms = [(p, 1/min(n_possibles,11)) for p in ascii_lowercase[15:min(15 + n_possibles, 26)]]
-    if possible_operators == None:
-        possible_operators = [(o, 1/3) for o in {'and', 'or', 'arrow'}]
+        possible_atoms = [(p, 1/min(n_possibles,11)) 
+                for p in ascii_lowercase[15:min(15 + n_possibles, 26)]]
+    # generate default operator probability if none given
+    if possible_binary_operators == None:
+        possible_binary_operators = [(o, 1/len(binary_operators)) for o in binary_operators]
 
     atoms_list = generate_symbol_list(n_atoms, possible_atoms)
-    operators_list = generate_symbol_list(n_atoms - 1, possible_operators) 
+    operators_list = generate_symbol_list(n_atoms - 1, possible_binary_operators) 
+    for operator in unary_operators:
+        number = random.randrange(0, n_atoms + 1)
+        operators_list.extend([operator for n in range(number)])
 
-    return construct_formula(atoms_list, operators_list, negation_probability)
+    return construct_formula(atoms_list, operators_list)
 
 def generate_symbol_list(n_symbols, possible_symbols):
     """
@@ -41,8 +49,6 @@ def generate_symbol_list(n_symbols, possible_symbols):
     returns a random list of n_symbols symbols from 
     possible_symbols
     """
-    print([item[1] for item in possible_symbols])
-    print(sum( [item[1] for item in possible_symbols]))
 
     if not abs(1 - sum([item[1] for item in possible_symbols])) < .01:
         raise ValueError('symbol occurance probabilities must sum to 1')
@@ -60,37 +66,36 @@ def generate_symbol_list(n_symbols, possible_symbols):
                 break
     return symbols 
 
-def construct_formula(atoms, operators, negation_probability): 
+def construct_formula(atoms, operators): 
     """
     Builds a random formula from a list of atoms and operators.
-    The order of the lists does not matter. negation_probability
-    determines the probability that any given subformula will
-    be negated.
+    The order of the lists does not matter. 
     """
-    if len(atoms) > len(operators) + 1:
-        raise ValueError('too many atomositions')
-    if len(atoms) < len(operators) + 1:
-        raise ValueError('too many operators')
+    print("atoms:", atoms)
+    print("operators:", operators)
+    for op in operators: 
+        if not op in {'not', 'or', 'and', 'arrow'}:
+            raise ValueError('unexpected operator')
 
-    subformulas = [] 
-    for p in atoms:
-        if random.random() > negation_probability:
-            subformulas.append(p)
-        else:
-            subformulas.append(('not', p))
-             
+    subformulas = atoms[:]
+   
     while not operators == []:
-        # pop a random operator and two random formulas
-        # from the subformula list
-        formula = (pop_random(operators), 
-                  pop_random(subformulas), pop_random(subformulas))
-        # with some probability, the new subformula is
-        # negated before being added back to the list
-        if random.random() > negation_probability:
-            formula = ('not', formula)
-
+        # pop a random operator and a number of random formulas
+        # matching its arity from the subformula list
+        op = pop_random(operators)
+        if op == 'not':
+            formula = (op, pop_random(subformulas))
+        elif op in {'and', 'or', 'arrow'}:
+            if len(subformulas) == 1:
+                raise ValueError('too many operators')
+            formula = (op, pop_random(subformulas), pop_random(subformulas))
+        print("created subformula:", formula)
         subformulas.append(formula)
+        print("subformula list:", subformulas)
 
+    if not len(subformulas) == 1:
+        raise ValueError('too many atoms')
+ 
     return subformulas[0]
 
 def pop_random(l):
