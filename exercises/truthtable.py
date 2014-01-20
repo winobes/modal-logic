5de18f6624161b8b4fa1584ff36247bf5f16992d
Bill Noble <winobes@gmail.com>
2014-01-20 17:51:31 +0100

    support for building truthtables from formulas or operators

--- exercises/truthtable.py
+++ exercises/truthtable.py
@@ -1,5 +1,7 @@
-from formula import Formula
+from formula import Formula, Operator, Language
+import itertools
 
+from formula import L
 class Truthtable:
     """
     t = Truthtable()
@@ -31,49 +33,35 @@ class Truthtable:
         (builds the truth table for the given operator)
     """
     # TODO: __eq__ method for truth tables
-    #       build truth tables from operators
     def __init__(self, arg1 = None, arg2 = None):
         self.atoms = None
         self.values = None
 
-        if arg1 == None:
-            if not arg2 == None:
-                raise ValueError('needs first argument if seccond is supplied')
-        else:
-            if arg2 == None:
-                if type(arg1) == Formula:
-                    self.atoms = sorted(list(formula.atomics()))
-                    self.values = self.__build_from_formula(arg1)
-                elif type(arg1) == Operator:
-                    
+        if arg2 == None:
+            if type(arg1) == Formula:
+                formula = arg1
+                self.atoms = sorted(list(formula.atomics()))
+            elif type(arg1) == Operator:
+                if arg1.arity < len('pqrstuv'):
+                    atom_names = [c for c in 'pqrstuv']
                 else:
-                    raise TypeError('truthtable can be build from Formula or \
-                                     Operator')
+                    atom_names = ['p_' + str(n) for n in range(arg1.arity)]
+                self.atoms = [L.build(name) for name in atom_names]
+                formula = L.build(arg1, *self.atoms)
             else:
-                self.atoms = arg1
-                self.values = arg2
+                raise TypeError('truthtable can only be built from Formula or \
+                                 Operator')
 
-    def __build_from_formula(self, f):
-        import itertools
-        valuetable = list(itertools.product([False, True], repeat=len(self.atoms)))
-        atomvalues = [ { p:row[self.atoms.index(p)] for p in self.atoms } for row in valuetable ]
-        return [ f.evaluate(values) for values in atomvalues ]
+            valuetable = list(itertools.product([False, True], 
+                                        repeat=len(self.atoms)))
+            atomvalues = [ {p:row[self.atoms.index(p)] for p in self.atoms } 
+                           for row in valuetable ]
+            self.values = [f.evaluate(values) for values in atomvalues]
+        else:
+            self.atoms = arg1
+            self.values = arg2
 
-    def 
-
-
-""" 
-TESTING
-"""
-
-import random
-from random_formula import generate_random_formula
-
-from formula import L
-
-f = Formula(L, "a->b") 
-t = Truthtable(f)
-
-print(f)
-print(t.atoms)
-print(t.values)
+    def is_equal(self, other):
+    # Assumes that self and other share all the same atomics.    
+    # TODO handle the case where they don't
+        return self.values == other.values
